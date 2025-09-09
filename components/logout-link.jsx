4ff8332ceba93af226f7c6881548bf465b481dd2
Daniel Kuznetsov <danilgkuznet@icloud.com>
2025-09-08 19:09:34 -0700

    Try another approach for log-outs

--- components/logout-link.jsx
+++ components/logout-link.jsx
@@ -2,16 +2,20 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { SignOutButton } from '@clerk/nextjs'
+// import { SignOutButton } from '@clerk/nextjs'
 import { LogOut } from 'lucide-react'
+import { useClerk } from '@clerk/nextjs'
 
 export default function LogoutLink() {
+    const { signOut } = useClerk()
+
     return (
-        <SignOutButton redirectUrl="/">
-            <Button type="button" variant="outline" className="w-full flex items-center justify-center gap-[6px]">
-                <LogOut size={14} className="mt-[1px]" />
-                <span className="leading-7">Sign out</span>
-            </Button>
-        </SignOutButton>
+        // <SignOutButton redirectUrl="/">
+        //     <Button type="button" variant="outline" className="w-full flex items-center justify-center gap-[6px]">
+        //         <LogOut size={14} className="mt-[1px]" />
+        //         <span className="leading-7">Sign out</span>
+        //     </Button>
+        // </SignOutButton>
+        <button onClick={() => signOut({ redirectUrl: '/' })}>Sign out</button>
     )
 }
